7d13abfcc5dfbea7961f78d353d09e94e9e04d0a
xiaonya <96672580+xiaonya@users.noreply.github.com>
2025-10-28 14:53:29 +0800

    更新 docusaurus.config.js
    https fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://xiaonya.github.io',
+  url: 'http://xiaonya.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '',
